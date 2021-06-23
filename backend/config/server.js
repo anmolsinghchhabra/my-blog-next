6a306e346d2a8826c234c68352508e2d258da44a
Anmol Singh Chhabra <writetoanmolsingh@gmail.com>
2021-06-23 16:43:25 +0530

    my setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a87bc2a03a5d46745384f6b0414259bf'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '86f32887-1cb8-405e-9f93-105ac8390fd0'
+};
