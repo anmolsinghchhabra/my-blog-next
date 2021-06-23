e173cee6826868bad2f2efbcbc54151c79ad1132
Anmol Singh Chhabra <writetoanmolsingh@gmail.com>
2021-06-23 17:41:01 +0530

    changes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "248fbe484e64e86e904e";
+/******/ 	var hotCurrentHash = "f15e86b021aed3599eba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
